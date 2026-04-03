e05f7597b2bb956f6d8ce7b3db188b859598b379
이교형 <kevin1371@naver.com>
2026-04-04 00:45:37 +0900

    feat: S3 파일 업로드 검증용 SelfStudyConfirmRequest DTO 추가

--- src/main/java/LDHD/project/common/aws/web/dto/SelfStudyConfirmRequest.java
+++ src/main/java/LDHD/project/common/aws/web/dto/SelfStudyConfirmRequest.java
@@ -1,4 +1,30 @@
 package LDHD.project.common.aws.web.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SelfStudyConfirmRequest {
+
+    @NotBlank(message = "제목은 필수입니다.")
+    private String title;
+
+    private String description;
+
+    @NotBlank(message = "s3Key는 필수입니다.")
+    @Schema(description = "Presigned URL 발급 시 반환된 s3Key")
+    private String s3Key;
+
+    @NotBlank(message = "원본 파일명은 필수입니다.")
+    private String originalFileName;
+
+    @NotNull(message = "파일 크기는 필수입니다.")
+    @Schema(description = "파일 크기 (bytes)")
+    private Long fileSize;
 }
